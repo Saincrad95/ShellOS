d48d5a84f317b03160052833f2101a02d6cbcb74
Austin Raab <austinraab95@gmail.com>
2020-09-06 01:26:22 -0500

    Fixed some basic commands

--- ShellOS/ShellOS/Kernel.cs
+++ ShellOS/ShellOS/Kernel.cs
@@ -7,18 +7,21 @@ namespace ShellOS
 {
     public class Kernel : Sys.Kernel
     {
+        public static Sys.FileSystem.CosmosVFS newFS = new Sys.FileSystem.CosmosVFS();
+
         protected override void BeforeRun()
         {
+            newFS.Initialize();
             Console.Clear();
             Console.WriteLine("ShellOS has booted successfully. Need help type 'help' as a command");
         }
 
         protected override void Run()
         {
-            Input();
+            InputCommands();
         }
 
-        public void Input(/*string arg*/) {
+        public void InputCommands(/*string arg*/) {
             Console.Write("\n$ ");
             string input = Console.ReadLine();
 
@@ -27,16 +30,47 @@ namespace ShellOS
                 Console.Write("Hello, World!");
             } else if (input == "help")
             {
-                Console.WriteLine("The available commands currently are help, SayHello, and " +
-                    "echo.");
+                Console.WriteLine("The available commands currently are help, SayHello, " +
+                    "echo, restart, and shutdown");
             } else if (input == "echo")
             {
                 Console.Write("What would you like to echo: ");
-                Console.ReadLine();
+                var echo = Console.ReadLine();
+                Console.WriteLine($"Echo: {echo}");
+            } else if (input == "restart" || input == "reboot")
+            {
+                Console.Write("Are you sure you want to restart? (Y/N): ");
+                var restart = Console.ReadLine();
+                switch (restart.ToUpper())
+                {
+                    case "Y":
+                        Sys.Power.Reboot();
+                        break;
+                    case "N":
+                        Console.WriteLine("Returning to the OS.");
+                        break;
+                    default:
+                        break;
+                }
+            } else if (input == "shutdown")
+            {
+                Console.Write("Are you sure you want to shutdown? (Y/N): ");
+                var shutdown = Console.ReadLine();
+                switch (shutdown.ToUpper())
+                {
+                    case "Y":
+                        Sys.Power.Shutdown();
+                        break;
+                    case "N":
+                        Console.WriteLine("Returning to the OS.");
+                        break;
+                    default:
+                        break;
+                }
             } else
             {
                 Console.WriteLine("Command not supported; Type 'help' for any help on supported" +
-                    "commands.");
+                    " commands.");
             }
         }
     }
